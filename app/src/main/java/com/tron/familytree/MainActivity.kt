fa8ca9582283347c7efc539840dc9917818dea0a
TronChen <dtp6284tj0@gmail.com>
2020-12-29 20:58:47 +0800

    Implement tool bar title changes when entering chatroom person name

--- app/src/main/java/com/tron/familytree/MainActivity.kt
+++ app/src/main/java/com/tron/familytree/MainActivity.kt
@@ -1,27 +1,18 @@
 package com.tron.familytree
 
-import android.R.attr
-import android.R.attr.data
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.FragmentActivity
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
 import com.github.dhaval2404.imagepicker.ImagePicker
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.analytics.FirebaseAnalytics
@@ -29,7 +20,6 @@ import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.ktx.Firebase
 import com.tron.familytree.data.User
 import com.tron.familytree.databinding.ActivityMainBinding
-import com.tron.familytree.databinding.NavHeaderDrawerBinding
 import com.tron.familytree.ext.getVmFactory
 import com.tron.familytree.message.MessageFragmentDirections
 import com.tron.familytree.util.CurrentFragmentType
@@ -55,29 +45,14 @@ class MainActivity : AppCompatActivity() {
         when (item.itemId) {
 
             R.id.navigation_branch -> {
-//                binding.fab.visibility = View.VISIBLE
-//                binding.imageSearch.visibility = View.VISIBLE
-//                binding.imageCalendar.visibility = View.INVISIBLE
-//                binding.imageScan.visibility = View.INVISIBLE
-//                binding.imageEdit.visibility = View.INVISIBLE
                 findNavController(R.id.myNavHostFragment).navigate(R.id.action_global_branchFragment)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_map-> {
-//                binding.fab.visibility = View.VISIBLE
-//                binding.imageSearch.visibility = View.INVISIBLE
-//                binding.imageCalendar.visibility = View.INVISIBLE
-//                binding.imageScan.visibility = View.INVISIBLE
-//                binding.imageEdit.visibility = View.INVISIBLE
                 findNavController(R.id.myNavHostFragment).navigate(R.id.action_global_mapsFragment)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_message -> {
-//                binding.fab.visibility = View.VISIBLE
-//                binding.imageSearch.visibility = View.INVISIBLE
-//                binding.imageCalendar.visibility = View.INVISIBLE
-//                binding.imageScan.visibility = View.INVISIBLE
-//                binding.imageEdit.visibility = View.INVISIBLE
                 findNavController(R.id.myNavHostFragment).navigate(MessageFragmentDirections.actionGlobalMessageFragment(
                     User(id = UserManager.email.toString(),
                     name = UserManager.name.toString())
@@ -85,11 +60,6 @@ class MainActivity : AppCompatActivity() {
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_profile -> {
-//                binding.fab.visibility = View.VISIBLE
-//                binding.imageSearch.visibility = View.INVISIBLE
-//                binding.imageCalendar.visibility = View.INVISIBLE
-//                binding.imageScan.visibility = View.VISIBLE
-//                binding.imageEdit.visibility = View.VISIBLE
                 findNavController(R.id.myNavHostFragment).navigate(R.id.action_global_profileFragment)
                 return@OnNavigationItemSelectedListener true
             }
@@ -104,10 +74,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-//        if (!Places.isInitialized()) {
-//            let { Places.initialize(this,"AIzaSyCiCPmbflbA9FlcM46aRJ4istK6GfAA2GQ") }
-//        }
-
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
@@ -115,13 +81,6 @@ class MainActivity : AppCompatActivity() {
         firebaseAnalytics = Firebase.analytics
 
         val navController = this.findNavController(R.id.myNavHostFragment)
-//        binding.fab.setOnClickListener {
-////            binding.imageSearch.visibility = View.INVISIBLE
-////            binding.imageCalendar.visibility = View.VISIBLE
-////            binding.imageScan.visibility = View.INVISIBLE
-////            binding.imageEdit.visibility = View.INVISIBLE
-//            navController.navigate(R.id.action_global_familyFragment)
-//        }
 
         binding.imageCalendar.setOnClickListener {
             navController.navigate(R.id.action_global_calendarDialog)
@@ -135,10 +94,6 @@ class MainActivity : AppCompatActivity() {
             navController.navigateUp()
         }
 
-//        binding.imageEdit.setOnClickListener {
-//            navController.navigate(R.id.action_global_editUserFragment)
-//        }
-
         // observe current fragment change, only for show info
         viewModel.currentFragmentType.observe(this, androidx.lifecycle.Observer {
             Log.i("Tron","~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
@@ -147,7 +102,6 @@ class MainActivity : AppCompatActivity() {
         })
 
         setupBottomNav()
-//        setupDrawer()
         setupNavController()
     }
 
@@ -165,7 +119,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.albumDetailFragment -> CurrentFragmentType.FAMILY_ALBUM
                 R.id.qrCodeFragment -> CurrentFragmentType.QR_CODE
                 R.id.qrCodeReaderFragment -> CurrentFragmentType.QR_CODE_SCAN
-                R.id.chatRoomFragment -> CurrentFragmentType.CHATROOM
+                R.id.chatRoomFragment -> CurrentFragmentType.CHAT_ROOM
                 R.id.logInFragment -> CurrentFragmentType.LOGIN
                 R.id.albumFragment -> CurrentFragmentType.FAMILY_ALBUM
 
@@ -179,110 +133,6 @@ class MainActivity : AppCompatActivity() {
     private fun setupBottomNav() {
         binding.bottomNavigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
     }
-
-
-//    private fun setupDrawer() {
-//
-//        // set up toolbar
-//        val navController = this.findNavController(R.id.myNavHostFragment)
-//        setSupportActionBar(binding.toolbar)
-//        supportActionBar?.title = null
-//
-//        appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerLayout)
-//        NavigationUI.setupWithNavController(binding.drawerNavView, navController)
-//
-//
-//        binding.drawerNavView.setNavigationItemSelectedListener {
-//            when(it.itemId){
-//                R.id.navigation_branch -> {
-////                    viewModel.navigate.value = 1
-//                    findNavController(R.id.myNavHostFragment).navigate(R.id.action_global_branchFragment)
-//                    binding.drawerLayout.closeDrawer(GravityCompat.START)
-//                    true
-//                }
-//                R.id.navigation_map->{
-////                    val bundle = bundleOf("type" to 0)
-//                    findNavController(R.id.myNavHostFragment).navigate(R.id.action_global_mapFragment)
-//                    binding.drawerLayout.closeDrawer(GravityCompat.START)
-////                    viewModel.currentFragmentType.value = CurrentFragmentType.READY_TO_SHIP
-//                    true
-//                }
-//                R.id.navigation_message->{
-////                    val bundle = bundleOf("type" to 1)
-//                    findNavController(R.id.myNavHostFragment).navigate(R.id.action_global_messageFragment)
-//                    binding.drawerLayout.closeDrawer(GravityCompat.START)
-////                    viewModel.currentFragmentType.value = CurrentFragmentType.SHIPPING
-//                    true
-//                }
-//                R.id.navigation_profile->{
-//                    findNavController(R.id.myNavHostFragment).navigate(R.id.action_global_profileFragment)
-//                    binding.drawerLayout.closeDrawer(GravityCompat.START)
-////                    viewModel.currentFragmentType.value = CurrentFragmentType.SHIPPED
-//                    true
-//                }
-//                else -> false
-//            }
-//        }
-//
-//        binding.drawerLayout.fitsSystemWindows = true
-//        binding.drawerLayout.clipToPadding = false
-//
-//        actionBarDrawerToggle = object : ActionBarDrawerToggle(
-//            this, binding.drawerLayout, binding.toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
-//            override fun onDrawerOpened(drawerView: View) {
-//                super.onDrawerOpened(drawerView)
-//
-////                when (UserManager.isLoggedIn) { // check user login status when open drawer
-////                    true -> {
-////                        viewModel.checkUser()
-////                    }
-////                    else -> {
-////                        findNavController(R.id.myNavHostFragment).navigate(NavigationDirections.navigateToLoginDialog())
-////                        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
-////                            binding.drawerLayout.closeDrawer(GravityCompat.START)
-////                        }
-////                    }
-////                }
-//
-//            }
-//        }.apply {
-//            binding.drawerLayout.addDrawerListener(this)
-//            syncState()
-//        }
-//
-//        // Set up header of drawer ui using data binding
-//        val bindingNavHeader = NavHeaderDrawerBinding.inflate(
-//            LayoutInflater.from(this), binding.drawerNavView, false)
-//
-//        bindingNavHeader.lifecycleOwner = this
-////        bindingNavHeader.viewModel = viewModel
-//        binding.drawerNavView.addHeaderView(bindingNavHeader.root)
-//
-////        viewModel.navigate.observe(this, Observer {
-////            //navigate到指定頁面並變動bottomNavigation
-////            binding.bottomNavView.selectedItemId = R.id.navigation_cart
-////        })
-//
-//        // Observe current drawer toggle to set the navigation icon and behavior
-////        viewModel.currentDrawerToggleType.observe(this, Observer { type ->
-////
-////            actionBarDrawerToggle?.isDrawerIndicatorEnabled = type.indicatorEnabled
-////            supportActionBar?.setDisplayHomeAsUpEnabled(!type.indicatorEnabled)
-////            binding.toolbar.setNavigationIcon(
-////                when (type) {
-////                    DrawerToggleType.BACK -> R.drawable.toolbar_back
-////                    else -> R.drawable.toolbar_menu
-////                }
-////            )
-////            actionBarDrawerToggle?.setToolbarNavigationClickListener {
-////                when (type) {
-////                    DrawerToggleType.BACK -> onBackPressed()
-////                    else -> {}
-////                }
-////            }
-////        })
-//    }
-
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
--- app/src/main/java/com/tron/familytree/util/CurrentFrafmentType.kt
+++ app/src/main/java/com/tron/familytree/util/CurrentFrafmentType.kt
@@ -1,17 +1,18 @@
 package com.tron.familytree.util
 
-enum class CurrentFragmentType(val value: String) {
+enum class CurrentFragmentType(var value: String) {
     BRANCH("家族樹"),
     FAMILY("家族活動"),
     FAMILY_EVENT("家族活動"),
     FAMILY_ALBUM("家族相簿"),
     MAPS("成員位置"),
     MESSAGE("聊天室"),
-    CHATROOM(""),
+    CHAT_ROOM(""),
     PROFILE("個人"),
     PROFILE_USER_EDIT("編輯個人資料"),
     LOGIN(""),
     CALENDAR("活動日期"),
     QR_CODE_SCAN("掃描QR code"),
-    QR_CODE("顯示QR code")
+    QR_CODE("顯示QR code"),
+
 }
